85f15ba02cebe42b6324063064e82131bb86319a
FERRACCI Andrea 505335 <andrea.ferracci@bnlmail.com>
2019-11-12 22:59:16 +0100

    add - Navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Recipes from "./pages/Recipes";
 import SingleRecipe from "./pages/SingleRecipe";
 import Default from "./pages/Default";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <main>
         {/* navbar */}
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/recipes" exact component={Recipes} />          
