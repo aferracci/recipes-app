83f33052cca85a276b0ad328e435b2f7b94ca072
FERRACCI Andrea 505335 <andrea.ferracci@bnlmail.com>
2019-11-12 21:49:52 +0100

    page structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import './App.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import Default from "./pages/Default";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div>hello from recipes app</div>
+
+    <Router>
+      <main>
+        {/* navbar */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/recipes" exact component={Recipes} />          
+          <Route path="/recipes/:id" component={SingleRecipe} />
+          <Route component={Default} />  
+        </Switch>
+      </main>
+    </Router>
+
   );
 }
 
